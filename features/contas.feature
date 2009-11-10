8c3ee371f8e59ba0dbe25ea64a3bed9032a40604
Luiz Aguiar <luiz.aguiar@gmail.com>
2009-11-10 00:50:22 -0200

    feature do cucumber para criar contas

---
+++ features/contas.feature
@@ -0,0 +1,9 @@
+Funcionalidade: Contas financeiras
+	Para poder acompanhar meus saldos financeiros
+	preciso cadastrar vários tipos de contas
+	
+	Cenario: Cadastro de contas
+		Dado que estou na tela de contas
+		E clico no link "New Conta"
+		E preencho o formulario
+		Entao devo ver a nova conta na lista de contas
---
+++ features/step_definitions/contas_steps.rb
@@ -0,0 +1,16 @@
+Dado /^que estou na tela de contas$/ do
+  visit "/contas"
+end
+
+E /^clico no link "([^\"]*)"$/ do |link|
+  click_link (link)
+end
+
+E /^preencho o formulario$/ do
+  fill_in "participante[nome]", :with => "Conta Bancário do João"
+  click_button("Create")
+end
+
+Entao /^devo ver a nova conta na lista de contas$/ do
+  response.should contain("Conta Bancário do João") 
+end
