514b952bd9ae7d7ba974a26cb7f53d6f060d4ed5
Luiz Aguiar <luiz.aguiar@gmail.com>
2010-01-07 15:12:14 -0200

    ajustada a gem do inherited_resources

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'josevalim-inherited_resources', :lib => 'inherited_resources', :source => 'http://gemcutter.org'
+  config.gem 'inherited_resources'
   config.gem 'paperclip'
   config.gem 'cucumber', :lib => false
   config.gem 'webrat', :lib => false
